190a623b2cb123dc5aef48462573d4f82781dfae
paolo2000@gmail.com <paolo2000@gmail.com@5d94ff98-d8ec-0eef-4e46-d71cdeb7696c>
2011-06-25 18:26:46 +0000

    settlebank 추가

---
+++ trunk/sports-score/site/head.php
@@ -0,0 +1,52 @@
+<?php
+// GR 보드 필수요소 정의 & 부르기
+include "include_board.php"; # 게시판 연동 부르기!
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sports Score</title>
+<style type="text/css">
+@import url(latest/sirini_latest5/style.css);
+@import url(outlogin/sirini_outlogin3/style.css);
+@import url(latest/sirini_total_search/style.css);
+</style>
+<script type="text/javascript" src="js/total_search.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/ss_common.css" media="all" />
+<!--[if IE]><link rel="stylesheet" type="text/css" href="../css/ss_ie.css" media="all" /><![endif]-->
+<script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="../js/ui.js"></script>
+</head>
+
+<body>
+
+<!-- header -->
+<div id="header">
+	<div id="head_in">
+		<h1><a href="#"><img src="../images/bi.gif" width="264" height="62" alt="스포츠스코어" /></a></h1>
+		<!-- SNB -->
+		<div id="snb">
+			<ul>
+				<li class="snb01"><a href="#">스포츠스코어</a></li>
+				<li class="snb02"><a href="#">스포츠TV</a></li>
+				<li class="snb03"><a href="#">스포츠분석</a></li>
+				<li class="snb04"><a href="#">스포츠경기추천</a></li>
+				<li class="snb05"><a href="#">사기피해사례</a></li>
+				<li class="snb06"><a href="#">커뮤니티</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+</div>
+
+<!-- wrap -->
+<div id="wrap">
+	<!-- 비주얼 -->
+	<div id="visual">
+		<img src="../images/visual.jpg" width="895" height="189" border="0" alt="" />
+	</div>
+	<!-- content -->
+	<div id="content">
