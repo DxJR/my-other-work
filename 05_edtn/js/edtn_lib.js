ebbcc542d272f7f2b612fb7545796de33f59f167
paolo2000 <paolo2000@5d94ff98-d8ec-0eef-4e46-d71cdeb7696c>
2012-03-16 09:34:12 +0000

    edtn 추가

---
+++ 05_edtn/js/edtn_lib.js
@@ -0,0 +1,65 @@
+jQuery(function($){
+	// Common
+	var select_root = $('div.select');
+	var select_value = $('.my_value');
+	var select_a = $('div.select>ul>li>a');
+	var select_input = $('div.select>ul>li>input[type=radio]');
+	var select_label = $('div.select>ul>li>label');
+
+	// Radio Default Value
+	$('div.my_value').each(function(){
+		var default_value = $(this).next('.i_list').find('input[checked]').next('label').text();
+		$(this).append(default_value);
+	});
+
+	// Line
+	select_value.bind('focusin',function(){$(this).addClass('outLine')});
+	select_value.bind('focusout',function(){$(this).removeClass('outLine')});
+	select_input.bind('focusin',function(){$(this).parents('div.select').children('div.my_value').addClass('outLine')});
+	select_input.bind('focusout',function(){$(this).parents('div.select').children('div.my_value').removeClass('outLine')});
+
+	// Show
+	function show_option(){
+		$(this).parents('div.select:first').toggleClass('open');
+	}
+
+	// Hover
+	function i_hover(){
+		$(this).parents('ul:first').children('li').removeClass('hover');
+		$(this).parents('li:first').toggleClass('hover');
+	}
+
+	// Hide
+	function hide_option(){
+		var t = $(this);
+		setTimeout(function(){
+			t.parents('div.select:first').removeClass('open');
+		}, 1);
+	}
+
+	// Set Input
+	function set_label(){
+		var v = $(this).next('label').text();
+		$(this).parents('ul:first').prev('.my_value').text('').append(v);
+		$(this).parents('ul:first').prev('.my_value').addClass('selected');
+	}
+
+	// Set Anchor
+	function set_anchor(){
+		var v = $(this).text();
+		$(this).parents('ul:first').prev('.my_value').text('').append(v);
+		$(this).parents('ul:first').prev('.my_value').addClass('selected');
+	}
+
+	// Anchor Focus Out
+	$('*:not("div.select a")').focus(function(){
+		$('.a_list').parent('.select').removeClass('open');
+	});
+
+	select_value.click(show_option);
+	select_root.removeClass('open');
+	select_root.mouseleave(function(){$(this).removeClass('open')});
+	select_a.click(set_anchor).click(hide_option).focus(i_hover).hover(i_hover);
+	select_input.change(set_label).focus(set_label);
+	select_label.hover(i_hover).click(hide_option);
+});
