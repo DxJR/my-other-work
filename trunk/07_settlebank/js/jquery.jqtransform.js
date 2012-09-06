fcb43c11fe3545dafdbe073c1fd075181c7d0472
paolo2000@gmail.com <paolo2000@gmail.com@5d94ff98-d8ec-0eef-4e46-d71cdeb7696c>
2012-09-06 07:58:30 +0000

    update

--- trunk/07_settlebank/js/jquery.jqtransform.js
+++ trunk/07_settlebank/js/jquery.jqtransform.js
@@ -8,10 +8,10 @@
 		var imgHover = new Image();
 		imgHover.src = strImgUrl.replace(/\.([a-zA-Z]*)$/,'-hover.$1');
 		var imgFocus = new Image();
-		imgFocus.src = strImgUrl.replace(/\.([a-zA-Z]*)$/,'-focus.$1');				
+		imgFocus.src = strImgUrl.replace(/\.([a-zA-Z]*)$/,'-focus.$1');
 	};
 
-	
+
 	/***************************
 	  Labels
 	***************************/
@@ -24,13 +24,13 @@
 				var inputname = objfield.attr('id');
 				if(inputname){
 					oLabel = selfForm.find('label[for="'+inputname+'"]');
-				} 
+				}
 			}
 		}
 		if(oLabel.is('label')){return oLabel.css('cursor','pointer');}
 		return false;
 	};
-	
+
 	/* Hide all open selects */
 	var jqTransformHideSelect = function(oTarget){
 		var ulVisible = $('.jqTransformSelectWrapper ul:visible');
@@ -48,8 +48,8 @@
 	/* Apply document listener */
 	var jqTransformAddDocumentListener = function (){
 		$(document).mousedown(jqTransformCheckExternalClick);
-	};	
-			
+	};
+
 	/* Add a new handler for the reset action */
 	var jqTransformReset = function(f){
 		var sel;
@@ -59,21 +59,21 @@
 	};
 
 	/***************************
-	  Radio Buttons 
-	 ***************************/	
+	  Radio Buttons
+	 ***************************/
 	$.fn.jqTransRadio = function(){
 		return this.each(function(){
 			if($(this).hasClass('jqTransformHidden')) {return;}
 
 			var $input = $(this);
 			var inputSelf = this;
-				
+
 			oLabel = jqTransformGetLabel($input);
 			oLabel && oLabel.click(function(){aLink.trigger('click');});
-	
+
 			var aLink = $('<a href="#" class="jqTransformRadio" rel="'+ this.name +'"></a>');
 			$input.addClass('jqTransformHidden').wrap('<span class="jqTransformRadioWrapper"></span>').parent().prepend(aLink);
-			
+
 			$input.change(function(){
 				inputSelf.checked && aLink.addClass('jqTransformChecked') || aLink.removeClass('jqTransformChecked');
 				return true;
@@ -82,23 +82,23 @@
 			aLink.click(function(){
 				if($input.attr('disabled')){return false;}
 				$input.trigger('click').trigger('change');
-	
+
 				// uncheck all others of same name input radio elements
 				$('input[name="'+$input.attr('name')+'"]',inputSelf.form).not($input).each(function(){
 					$(this).attr('type')=='radio' && $(this).trigger('change');
 				});
-	
-				return false;					
+
+				return false;
 			});
 			// set the default state
 			inputSelf.checked && aLink.addClass('jqTransformChecked');
 		});
 	};
-	
-	
+
+
 	/***************************
-	  Select 
-	 ***************************/	
+	  Select
+	 ***************************/
 	$.fn.jqTransSelect = function(){
 		return this.each(function(index){
 			var $select = $(this);
@@ -114,7 +114,7 @@
 				.parent()
 				.css({zIndex: 10-index})
 			;
-			
+
 			/* Now add the html for the select */
 			$wrapper.prepend('<div><span></span><a href="#" class="jqTransformSelectOpen"></a></div><ul></ul>');
 			var $ul = $('ul', $wrapper).css('width',$select.width()).hide();
@@ -123,11 +123,11 @@
 				var oLi = $('<li><a href="#" index="'+ i +'">'+ $(this).html() +'</a></li>');
 				$ul.append(oLi);
 			});
-			
+
 			/* Add click handler to the a */
 			$ul.find('a').click(function(){
 					$('a.selected', $wrapper).removeClass('selected');
-					$(this).addClass('selected');	
+					$(this).addClass('selected');
 					/* Fire the onchange event */
 					if ($select[0].selectedIndex != $(this).attr('index') && $select[0].onchange) { $select[0].selectedIndex = $(this).attr('index'); $select[0].onchange(); }
 					$select[0].selectedIndex = $(this).attr('index');
@@ -140,15 +140,15 @@
 			$('span:first', $wrapper).click(function(){$("a.jqTransformSelectOpen",$wrapper).trigger('click');});
 			oLabel && oLabel.click(function(){$("a.jqTransformSelectOpen",$wrapper).trigger('click');});
 			this.oLabel = oLabel;
-			
+
 			/* Apply the click handler to the Open */
 			var oLinkOpen = $('a.jqTransformSelectOpen', $wrapper)
 				.click(function(){
 					//Check if box is already open to still allow toggle, but close all other selects
-					if( $ul.css('display') == 'none' ) {jqTransformHideSelect();} 
+					if( $ul.css('display') == 'none' ) {jqTransformHideSelect();}
 					if($select.attr('disabled')){return false;}
 
-					$ul.slideToggle('fast', function(){					
+					$ul.slideToggle('fast', function(){
 						var offSet = ($('a.selected', $ul).offset().top - $ul.offset().top);
 						$ul.animate({scrollTop: offSet});
 					});
@@ -163,32 +163,31 @@
 			$wrapper.css('width',newWidth);
 			$ul.css('width',newWidth-2);
 			oSpan.css({width:iSelectWidth});
-		
+
 			// Calculate the height if necessary, less elements that the default height
 			//show the ul to calculate the block, if ul is not displayed li height value is 0
 			$ul.css({display:'block',visibility:'hidden'});
 			var iSelectHeight = ($('li',$ul).length)*($('li:first',$ul).height());//+1 else bug ff
 			(iSelectHeight < $ul.height()) && $ul.css({height:iSelectHeight,'overflow':'hidden'});//hidden else bug with ff
 			$ul.css({display:'none',visibility:'visible'});
-			
+
 		});
 	};
 	$.fn.jqTransform = function(options){
 		var opt = $.extend({},defaultOptions,options);
-		
+
 		/* each form */
 		 return this.each(function(){
 			var selfForm = $(this);
 			if(selfForm.hasClass('jqtransformdone')) {return;}
-			selfForm.addClass('jqtransformdone');	
+			selfForm.addClass('jqtransformdone');
 
-			$('input:radio', this).jqTransRadio();			
+			$('input:radio', this).jqTransRadio();
 			if( $('select', this).jqTransSelect().length > 0 ){jqTransformAddDocumentListener();}
 			selfForm.bind('reset',function(){var action = function(){jqTransformReset(this);}; window.setTimeout(action, 10);});
 
 		}); /* End Form each */
-				
+
 	};/* End the Plugin */
 
 })(jQuery);
-				   
