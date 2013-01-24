1882361e081ff5851de9fc289a07bc99dd36ad14
paolo2000@gmail.com <paolo2000@gmail.com@5d94ff98-d8ec-0eef-4e46-d71cdeb7696c>
2013-01-24 05:04:57 +0000

    update

--- trunk/08_inmoonTV/js/inmoontv.js
+++ trunk/08_inmoonTV/js/inmoontv.js
@@ -6,6 +6,8 @@ var imTv = ({
 	init : function() {
 		this.swipeInit("visualSwipe", ".visual_by", "500");
 		this.swipeInit("swipeLecture", ".lecture_list", ".lecture_swipe_w", "500");
+		this.lectureDetailView();
+		this.lectureListDetailView();
 	},
 
 	swipeInit : function(obj, inElement, inElementWidthTarget, speed) {
@@ -27,7 +29,9 @@ var imTv = ({
 			preventDefault: true,
 			swipeLeft: function() {
 				currentSwipeObj = Math.max(currentSwipeObj + 1, 0);
-				this.swipeAnimation(swipeObjWidth * currentSwipeObj, swipeSpeed);
+				if (currentSwipeObj < maxISwipeObj) {
+					this.swipeAnimation(swipeObjWidth * currentSwipeObj, swipeSpeed);
+				}
 			},
 			swipeRight: function() {
 				currentSwipeObj = Math.max(currentSwipeObj - 1, 0);
@@ -38,6 +42,97 @@ var imTv = ({
 				var value = (distance<0 ? "" : "-") + Math.abs(distance).toString();
 				swipeObj.css("-webkit-transform", "translate3d("+value +"px,0px,0px)");
 			}
+		});
+	},
+	// 강의 상세보기
+	lectureDetailView : function() {
+		var tabMenu = $(".ld_tab");
+		var tabBorder = $(".bdb");
+		var tabContentInfo = $("#ldInfo");
+		var tabContentList = $("#ldList");
+		var tabContentToggle = function() {
+			if (tabMenu.find("li").filter(".this").index()==0) {
+				tabContentInfo.css("display","block");
+				tabContentList.css("display","none");
+			} else if (tabMenu.find("li").filter(".this").index()==1) {
+				tabContentInfo.css("display","none");
+				tabContentList.css("display","block");
+			}
+		}
+
+		tabContentToggle();
+
+		tabMenu.find("a").each(function(index) {
+			$(this).bind("click", function() {
+				if ($(this).parent().attr("class") !== "this") {
+					tabMenu.find("li").removeClass("this");
+					$(this).parent().addClass("this");
+					if (index==0) {
+						tabBorder.animate({"left" : "0"}, {duration:300, queue:true});
+					} else if (index==1) {
+						tabBorder.animate({"left" : "50%"}, {duration:300, queue:true});
+					}
+				}
+				tabContentToggle();
+				return false;
+			});
+		});
+	},
+	// 개별강의 상세
+	lectureListDetailView : function() {
+		var lectureListWrap = $("#ldList");
+		var lectureTitle = lectureListWrap.find("h5");
+		var lectureControlTitle =$(".ld_list_ct");
+
+		lectureTitle.each(function(index) {
+			$(this).bind("click", function() {
+				var listDetailWrap = $(".ld_list_d_w:eq("+index+")");
+
+				if (listDetailWrap.attr("class")=="ld_list_d_w ld_list_d_w_open") {
+					listDetailWrap.animate({"height" : "0px"}, {duration:300, queue:true, complete:function() {
+						listDetailWrap.removeClass("ld_list_d_w_open");
+						lectureTitle.removeClass("ld_list_in_this");
+					}});
+				} else {
+					$(".ld_list_d_w").each(function() {
+						$(this).css("height","0").removeClass("ld_list_d_w_open");
+					});
+					$(".ld_list_fnc").each(function() {
+						$(this).css("height","0").removeClass("ld_list_fnc_open");
+					});
+					lectureTitle.each(function() {
+						$(this).removeClass("ld_list_in_this");
+					});
+					listDetailWrap.animate({"height" : ""+$(".ld_list_d:eq("+index+")").height()+"px"}, {duration:300, queue:true, complete:function() {
+						listDetailWrap.addClass("ld_list_d_w_open");
+						listDetailWrap.css("height","auto");
+					}});
+					$(this).addClass("ld_list_in_this");
+				}
+			})
+		});
+		lectureControlTitle.each(function(index) {
+			$(this).bind("click", function() {
+				var lectureFunctionWrap = $(".ld_list_fnc:eq("+index+")");
+
+				if (lectureFunctionWrap.attr("class")=="ld_list_fnc ld_list_fnc_open") {
+					lectureFunctionWrap.animate({"height" : "0px"}, {duration:300, queue:true, complete:function() {
+						lectureFunctionWrap.removeClass("ld_list_fnc_open");
+						lectureFunctionWrap.prev().removeClass("ld_list_ct_this");
+					}});
+				} else {
+					$(".ld_list_fnc").each(function() {
+						$(this).css("height","0").removeClass("ld_list_fnc_open");
+					});
+					$(".ld_list_ct").each(function() {
+						$(this).removeClass("ld_list_ct_this");
+					});
+					lectureFunctionWrap.animate({"height" : "100px"}, {duration:300, queue:true, complete:function() {
+						lectureFunctionWrap.addClass("ld_list_fnc_open");;
+					}});
+					lectureFunctionWrap.prev().addClass("ld_list_ct_this");
+				}
+			});
 		});
 	}
 });
